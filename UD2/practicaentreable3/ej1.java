f1910817bcd27aa6e246b015900ca1e071ff98ec
Mauro Contador Alfonso <mconalf2108@g.educaand.es>
2025-11-07 09:42:55 +0100

    ej1

---
+++ UD2/practicaentreable3/ej1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package BOLETIN1;
+import java.util.Scanner;
+/**
+ *
+ * @author 1DAW-mconalf2108
+ */
+public class ej1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        eco(5);
+    }
+        
+        Scanner sc =new Scanner(System.in);
+    
+    /**Actividad 1: Diseñar una función eco() a la que se le pasa como parámetro un número n, 
+     * y muestra por pantalla n veces el mensaje "Eco..." **/
+    
+    public static void eco(int n){
+            for (int i = 0; i < n; i++) {
+                System.out.println("Eco...");
+            }
+          
+        }
+    
+    
+    }
+
+
+
